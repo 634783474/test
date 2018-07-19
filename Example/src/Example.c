9ff52bf040b6fe338546b6a127a18191d8798622
KelvinDeng <634783474@qq.com>
2018-07-19 23:37:01 +0800

    test1

--- Example/src/Example.c
+++ Example/src/Example.c
@@ -13,7 +13,7 @@
 
 int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	puts("this is a test for git.");
+	//puts("this is a test for git.");
 	//puts("third modify for test.");
 	//puts("cloud ");
 	return EXIT_SUCCESS;
