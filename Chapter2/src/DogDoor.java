9e0bdbe39e6a225f9a00c4c1eda6cb91a2f7e320
KelvinDeng <634783474@qq.com>
2018-07-29 23:31:07 +0800

    Chapter2 completed.

--- Chapter2/src/DogDoor.java
+++ Chapter2/src/DogDoor.java
@@ -1,14 +1,27 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DogDoor {
 	private boolean open;
-	
+	private List allowedBarks;
 	public DogDoor(){
+		this.allowedBarks=new LinkedList();
 		this.open=false;
 	}
 	
 	public void open() {
 		System.out.println("The dog door opens. ");
 		open=true;
+		
+		final Timer timer=new Timer();
+		timer.schedule(new TimerTask() {
+			public void run() {
+				close();
+				timer.cancel();
+			}
+		}, 5000);
 	}
 	
 	public void close(){
@@ -18,6 +31,14 @@ public class DogDoor {
 	
 	public boolean isOpen() {
 		return open;
+	}
+	
+	public void addAllowedBark(Bark bark) {
+		allowedBarks.add(bark);
+	}
+	
+	public List getAllowedBarks() {
+		return allowedBarks;
 	}
 
 }
